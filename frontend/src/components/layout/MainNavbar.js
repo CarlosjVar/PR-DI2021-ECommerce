7cb5b20db6c0a8929af922160f5bd85ef9632756
aguilarandress <andres.aguilarmoya2001@gmail.com>
2021-05-17 16:59:42 -0600

    footer and navbar styled

--- frontend/src/components/layout/MainNavbar.js
+++ frontend/src/components/layout/MainNavbar.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 
 const MainNavbar = () => {
   return (
-    <Navbar bg="primary" navbar="dark" expand="md">
+    <Navbar bg="primary" className="navbar-dark" expand="md">
       <Container>
-        <Navbar.Brand className="text-white" href="#">
-          CompuHardware
-        </Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand className="text-white">CompuHardware</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle
+          className="text-white"
+          aria-controls="responsive-navbar"
+        />
+        <Navbar.Collapse id="responsive-navbar">
+          <Nav className="ml-auto">
+            <Nav.Item>
+              <Link to="/" className="nav-link">
+                <i className="fa fa-home"></i> Home
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link to="/login" className="nav-link">
+                <i className="fa fa-sign-in"></i> Login
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link to="/register" className="nav-link">
+                <i className="fa fa-user-add"></i> Register
+              </Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
