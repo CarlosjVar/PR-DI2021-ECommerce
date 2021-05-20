eb5455d3357f49a6e5dd7fee414fe59a11bc1022
aguilarandress <andres.aguilarmoya2001@gmail.com>
2021-05-19 23:18:18 -0600

    reorganized reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,9 +3,12 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import authReducer from './reducers/authReducer';
-import clientReducer from './reducers/clientReducer';
+import alertReducer from './reducers/alertReducer';
 
-const reducer = combineReducers({ auth: authReducer, client: clientReducer });
+const reducer = combineReducers({
+  auth: authReducer,
+  alert: alertReducer,
+});
 
 const initialState = {};
 
