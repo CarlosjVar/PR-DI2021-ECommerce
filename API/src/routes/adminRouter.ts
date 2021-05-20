ff8b85ee217f9bd2de91fe82472980cdb12e9ce5
Carlos José Varela <carlosjvare@gmail.com>
2021-05-19 21:10:24 -0600

    Administrator Authentication and Creation

---
+++ API/src/routes/adminRouter.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import expressValidator, { body, validationResult } from 'express-validator';
+import isAuthenticatedAdmin from '../middleware/isAuthenticatedAdmin'
+import {createAdmin} from '../controllers/adminController'
+const adminRouter = express.Router();
+
+adminRouter.route('/create').post(
+    [
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('fullname').isLength({min:10}).withMessage('Please enter a full name with a lenght at least 8 characters long'),
+    body('password').isStrongPassword().withMessage('Please enter a stronger password'),
+    isAuthenticatedAdmin
+    ],
+    createAdmin)
+
+
+export default adminRouter
--- API/src/routes/productsRouter.ts
+++ API/src/routes/productsRouter.ts
@@ -1,4 +1,5 @@
-import express from 'express'
+import express, { Request } from 'express'
+import prismaController from '../config/Database'
 
 const productsRouter = express.Router();
 
@@ -8,4 +9,35 @@ productsRouter.route('/').get(
         console.log("Hola products");
     }
 )
+productsRouter.route('/create').post(()=>
+{
+
+}
+)
+productsRouter.route('/get').get((req:Request,res:Response)=>
+{
+    const {category, productname} = req.params
+
+    if(category == 'All' && productname != null)
+    {
+        prismaController.products.findMany({
+            where:{
+                    name:productname
+            }
+        })
+    }
+    else if(category != 'All' && productname != null)
+    {
+        prismaController.products.findMany({
+            where:{
+                    name:productname
+            },
+            include:
+            {
+                Categories:true
+            }
+        })
+    }
+    
+})
 export default productsRouter
