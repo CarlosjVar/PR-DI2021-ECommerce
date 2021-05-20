f6dc110e76d82bbc9b4941a151a58d0ab6c752ce
aguilarandress <andres.aguilarmoya2001@gmail.com>
2021-05-19 23:36:05 -0600

    register action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import setAuthenticationToken from './utils/setAuthenticationToken';
+import { logoutUser } from './actions/authActions';
+import store from './store';
 
 import MainNavbar from './components/layout/MainNavbar';
 import MainFooter from './components/layout/MainFooter';
 import Routes from './components/routing/Routes';
 
 const App = () => {
+  useEffect(() => {
+    if (localStorage.token) {
+      setAuthenticationToken(localStorage.token);
+    }
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) {
+        store.dispatch(logoutUser());
+      }
+      window.addEventListener('storage', () => {
+        if (!localStorage.token) {
+          store.dispatch(logoutUser());
+        }
+      });
+    });
+  }, []);
+
   return (
     <Router>
       <MainNavbar />
