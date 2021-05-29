ce9c67595101bf1364b7708a6d3fe534be839883
aguilarandress <andres.aguilarmoya2001@gmail.com>
2021-05-29 13:54:00 -0600

    implemented get products for home page

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const { productList } = useSelector((state) => state.product);
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>
