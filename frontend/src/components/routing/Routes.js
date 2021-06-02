4dd98468e2187783d872beb47dbef93dbb4ba4a3
aguilarandress <andres.aguilarmoya2001@gmail.com>
2021-06-02 14:34:10 -0600

    refactored react routes

--- frontend/src/components/routing/Routes.js
+++ frontend/src/components/routing/Routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
 
 import AdminRoute from './AdminRoute';
 import ClientRoute from './ClientRoute';
@@ -16,41 +15,31 @@ import RegisterAdminScreen from '../../screens/admin/register-admin/RegisterAdmi
 import AddProductScreen from '../../screens/admin/add-product/AddProductScreen';
 import AdminProductDetailsScreen from '../../screens/admin/admin-product-details/AdminProductDetailsScreen';
 import EditProductScreen from '../../screens/admin/edit-product/EditProductScreen';
-import Alert from '../layout/Alert';
 
 const Routes = () => {
   return (
     <>
       <Switch>
         <Route exact path="/" component={HomeScreen} />
+        <Route path="/login" component={LoginScreen} exact />
+        <Route path="/register" component={RegisterScreen} exact />
+        <ClientRoute path="/cart" component={ShoppingCartScreen} exact />
+        <ClientRoute path="/my-orders" component={OrdersScreen} exact />
+        <AdminRoute path="/dashboard" component={DashboardScreen} exact />
+        <AdminRoute path="/admins" component={AdminsScreen} exact />
+        <AdminRoute path="/admins/add" component={RegisterAdminScreen} exact />
+        <AdminRoute path="/products/add" component={AddProductScreen} exact />
+        <AdminRoute
+          path="/products/:id"
+          component={AdminProductDetailsScreen}
+          exact
+        />
+        <AdminRoute
+          path="/products/:id/edit"
+          component={EditProductScreen}
+          exact
+        />
       </Switch>
-      <Container className="pt-4">
-        <Alert />
-        <Switch>
-          <Route path="/login" component={LoginScreen} exact />
-          <Route path="/register" component={RegisterScreen} exact />
-          <ClientRoute path="/cart" component={ShoppingCartScreen} exact />
-          <ClientRoute path="/my-orders" component={OrdersScreen} exact />
-          <AdminRoute path="/dashboard" component={DashboardScreen} exact />
-          <AdminRoute path="/admins" component={AdminsScreen} exact />
-          <AdminRoute
-            path="/admins/add"
-            component={RegisterAdminScreen}
-            exact
-          />
-          <AdminRoute path="/products/add" component={AddProductScreen} exact />
-          <AdminRoute
-            path="/products/:id"
-            component={AdminProductDetailsScreen}
-            exact
-          />
-          <AdminRoute
-            path="/products/:id/edit"
-            component={EditProductScreen}
-            exact
-          />
-        </Switch>
-      </Container>
     </>
   );
 };
