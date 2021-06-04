de2de25ee3f33bf0227da9badf69a77f1e4419d1
aguilarandress <andres.aguilarmoya2001@gmail.com>
2021-06-04 14:32:42 -0600

    added containers to shared screens

--- frontend/src/screens/shared/LoginScreen.js
+++ frontend/src/screens/shared/LoginScreen.js
@@ -8,8 +8,11 @@ import {
   InputGroup,
   FormControl,
   Button,
+  Container,
 } from 'react-bootstrap';
 import { authenticateUser } from '../../actions/authActions';
+
+import Alert from '../../components/layout/Alert';
 
 const LoginScreen = () => {
   const dispatch = useDispatch();
@@ -35,63 +38,66 @@ const LoginScreen = () => {
   const { email, password } = formData;
 
   return (
-    <Row>
-      <Col md="6" className="mx-auto">
-        <Card>
-          <Card.Body>
-            <h3 className="text-center mt-3 mb-4">
-              <i className="fas fa-sign-in-alt"></i> Login
-            </h3>
-            <form onSubmit={onLoginSubmit}>
-              {/* Email Input */}
-              <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="basic-addon1">
-                    <i className="fa fa-envelope"></i>
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder="Email"
-                  type="email"
-                  name="email"
-                  value={email}
-                  onChange={onInputChange}
-                  aria-label="email"
-                  aria-describedby="basic-addon1"
-                />
-              </InputGroup>
-              {/* Password input */}
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="password-addon">
-                    <i className="fa fa-asterisk"></i>
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder="Password"
-                  type="password"
-                  name="password"
-                  value={password}
-                  onChange={onInputChange}
-                  aria-label="password"
-                  aria-describedby="password-addon"
-                />
-              </InputGroup>
-
-              <div className="link-wrapper">
-                <Link id="register-link" to="/register">
-                  Registrarme
-                </Link>
-              </div>
-
-              <Button type="submit" bg="primary" className="btn-block">
+    <Container className="py-5">
+      <Alert />
+      <Row>
+        <Col md="6" className="mx-auto">
+          <Card>
+            <Card.Body>
+              <h3 className="text-center mt-3 mb-4">
                 <i className="fas fa-sign-in-alt"></i> Login
-              </Button>
-            </form>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
+              </h3>
+              <form onSubmit={onLoginSubmit}>
+                {/* Email Input */}
+                <InputGroup className="mb-3">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="basic-addon1">
+                      <i className="fa fa-envelope"></i>
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={onInputChange}
+                    aria-label="email"
+                    aria-describedby="basic-addon1"
+                  />
+                </InputGroup>
+                {/* Password input */}
+                <InputGroup>
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="password-addon">
+                      <i className="fa fa-asterisk"></i>
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                    placeholder="Password"
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={onInputChange}
+                    aria-label="password"
+                    aria-describedby="password-addon"
+                  />
+                </InputGroup>
+
+                <div className="link-wrapper">
+                  <Link id="register-link" to="/register">
+                    Registrarme
+                  </Link>
+                </div>
+
+                <Button type="submit" bg="primary" className="btn-block">
+                  <i className="fas fa-sign-in-alt"></i> Login
+                </Button>
+              </form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- frontend/src/screens/shared/RegisterScreen.js
+++ frontend/src/screens/shared/RegisterScreen.js
@@ -9,9 +9,12 @@ import {
   FormControl,
   Button,
   Form,
+  Container,
 } from 'react-bootstrap';
 import { registerClient } from '../../actions/authActions';
 import { showAlert } from '../../actions/alertActions';
+
+import Alert from '../../components/layout/Alert';
 
 const RegisterScreen = () => {
   const history = useHistory();
@@ -37,7 +40,7 @@ const RegisterScreen = () => {
     if (!acceptTermsAndConditions) {
       dispatch(
         showAlert({
-          message: 'Por acepte los términos y condiciones',
+          message: 'Por favor acepte los términos y condiciones',
           type: 'danger',
         })
       );
@@ -60,106 +63,109 @@ const RegisterScreen = () => {
   const { email, fullname, password, passwordConfirmation } = formData;
 
   return (
-    <Row>
-      <Col md="6" className="mx-auto">
-        <Card>
-          <Card.Body>
-            <h4 className="text-center mt-3 mb-4">
-              <i className="fas fa-user-plus"></i> Registrarse
-            </h4>
-            <form onSubmit={onRegisterSubmit}>
-              {/* Email Input */}
-              <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="email-addon">
-                    <i className="fa fa-envelope"></i>
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder="Email"
-                  type="email"
-                  name="email"
-                  value={email}
-                  onChange={onInputChange}
-                  aria-label="email"
-                  aria-describedby="email-addon"
-                />
-              </InputGroup>
-              {/* Full name */}
-              <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="fullName-addon">
-                    <i className="fa fa-user"></i>
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder="Nombre y apellidos"
-                  type="text"
-                  name="fullname"
-                  value={fullname}
-                  onChange={onInputChange}
-                  aria-label="fullname"
-                  aria-describedby="fullName-addon"
-                />
-              </InputGroup>
-              {/* Password input */}
-              <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="password-addon">
-                    <i className="fa fa-asterisk"></i>
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder="Contraseña"
-                  type="password"
-                  name="password"
-                  value={password}
-                  onChange={onInputChange}
-                  aria-label="password"
-                  aria-describedby="password-addon"
-                />
-              </InputGroup>
-              {/* Password confirmation */}
-              <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <InputGroup.Text id="passwordConfirmation-addon">
-                    <i className="fa fa-asterisk"></i>
-                  </InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder="Confirmar contraseña"
-                  type="password"
-                  name="passwordConfirmation"
-                  value={passwordConfirmation}
-                  onChange={onInputChange}
-                  aria-label="passwordConfirmation"
-                  aria-describedby="passwordConfirmation-addon"
-                />
-              </InputGroup>
-              {/* Accept terms and conditions */}
-              <Form.Group controlId="acceptTermsAndConditions">
-                <Form.Check
-                  type="checkbox"
-                  label="Acepto los terminos y condiciones"
-                  className="text-center my-4"
-                  onChange={() =>
-                    setAcceptTermsAndConditions(!acceptTermsAndConditions)
-                  }
-                />
-              </Form.Group>
-              <Button
-                disabled={!acceptTermsAndConditions}
-                type="submit"
-                bg="primary"
-                className="btn-block"
-              >
-                <i className="fa fa-user-plus"></i> Registrarse
-              </Button>
-            </form>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
+    <Container className="py-5">
+      <Alert />
+      <Row>
+        <Col md="6" className="mx-auto">
+          <Card>
+            <Card.Body>
+              <h4 className="text-center mt-3 mb-4">
+                <i className="fas fa-user-plus"></i> Registrarse
+              </h4>
+              <form onSubmit={onRegisterSubmit}>
+                {/* Email Input */}
+                <InputGroup className="mb-3">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="email-addon">
+                      <i className="fa fa-envelope"></i>
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={onInputChange}
+                    aria-label="email"
+                    aria-describedby="email-addon"
+                  />
+                </InputGroup>
+                {/* Full name */}
+                <InputGroup className="mb-3">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="fullName-addon">
+                      <i className="fa fa-user"></i>
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                    placeholder="Nombre y apellidos"
+                    type="text"
+                    name="fullname"
+                    value={fullname}
+                    onChange={onInputChange}
+                    aria-label="fullname"
+                    aria-describedby="fullName-addon"
+                  />
+                </InputGroup>
+                {/* Password input */}
+                <InputGroup className="mb-3">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="password-addon">
+                      <i className="fa fa-asterisk"></i>
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                    placeholder="Contraseña"
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={onInputChange}
+                    aria-label="password"
+                    aria-describedby="password-addon"
+                  />
+                </InputGroup>
+                {/* Password confirmation */}
+                <InputGroup className="mb-3">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="passwordConfirmation-addon">
+                      <i className="fa fa-asterisk"></i>
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                    placeholder="Confirmar contraseña"
+                    type="password"
+                    name="passwordConfirmation"
+                    value={passwordConfirmation}
+                    onChange={onInputChange}
+                    aria-label="passwordConfirmation"
+                    aria-describedby="passwordConfirmation-addon"
+                  />
+                </InputGroup>
+                {/* Accept terms and conditions */}
+                <Form.Group controlId="acceptTermsAndConditions">
+                  <Form.Check
+                    type="checkbox"
+                    label="Acepto los terminos y condiciones"
+                    className="text-center my-4"
+                    onChange={() =>
+                      setAcceptTermsAndConditions(!acceptTermsAndConditions)
+                    }
+                  />
+                </Form.Group>
+                <Button
+                  disabled={!acceptTermsAndConditions}
+                  type="submit"
+                  bg="primary"
+                  className="btn-block"
+                >
+                  <i className="fa fa-user-plus"></i> Registrarse
+                </Button>
+              </form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
