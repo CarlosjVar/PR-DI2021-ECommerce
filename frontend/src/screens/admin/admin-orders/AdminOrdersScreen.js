1d1cde9e3729ffb9b35727e5cbffd7006a5f83b0
aguilarandress <andres.aguilarmoya2001@gmail.com>
2021-06-07 18:23:50 -0600

    fetch orders from server

--- frontend/src/screens/admin/admin-orders/AdminOrdersScreen.js
+++ frontend/src/screens/admin/admin-orders/AdminOrdersScreen.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllOrders } from '../../../actions/orderActions';
 
 const AdminOrdersScreen = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllOrders());
+  }, [dispatch]);
+
+  const { orderList } = useSelector((state) => state.order);
+
+  console.log(orderList);
+
   return (
     <div>
       <h2>AdminOrders</h2>
